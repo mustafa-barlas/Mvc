af3cba45dafd870017ea3572a9b98d4b40983bfa
mustafa barlas <mustafa.barlas1146@gmail.com>
2022-06-14 12:10:42 +0300

    ass
    sds

---
+++ BusinessLayer/Abstract/IContentService.cs
@@ -0,0 +1,20 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IContentService
+    {
+        List<Content> GetList();
+        List<Content> GetListByWriter(int id);
+        Content GetByID(int id);
+        List<Content> GetListByHeadingID(int id);
+        void ContentAdd(Content content);
+        void ContentDelete(Content content);
+        void ContentUpdate(Content content);
+    }
+}
---
+++ BusinessLayer/Abstract/IHeadingService.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Abstract
+{
+    public interface IHeadingService
+    {
+        List<Heading> GetList();
+        List<Heading> GetListByWriter();
+        Heading GetByID(int id); 
+        void HeadingDelete(Heading heading);
+        void HeadingUpdate(Heading heading);
+        void HeadingAdd(Heading heading);
+    }
+}
